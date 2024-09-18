97ab0759de9e2c80d77f3dc8555e08b85a6c2b06
testuser-aaa <93967440+testuser-aaa@users.noreply.github.com>
2024-09-18 14:29:07 +0500

    Update port_scan.py
    add -help options

--- port_scan.py
+++ port_scan.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 #немного докрученый сканер портов из книги Python for Cybersecurity.Using Python for Cyber Offense and Defense / Howard E. Poston III
-#по умолчанию сканирует по большому списку
+#по умолчанию сканирует по малому списку
+import re
 from scapy.all import *
 import argparse
 from colorama import Fore
 
-ports_big = [20, 21, 22, 23, 25, 42, 43, 53, 67, 69, 80, 110, 115, 123, 137, 138, 139, 143, 161, 179, 443, 445, 514, 515, 993, 995, 1080, 1194, 1433, 1702, 1723, 3128, 3268, 3306, 3389, 5432, 5060, 5900, 5938, 8080, 10000, 10050, 20000]
-ports_small=[25,80,53,443,445,3389, 8080,8443]
+big = [20, 21, 22, 23, 25, 42, 43, 53, 67, 69, 80, 110, 115, 123, 137, 138, 139, 143, 161, 179, 443, 445, 514, 515, 993, 995, 1080, 1194, 1433, 1702, 1723, 3128, 3268, 3306, 3389, 5432, 5060, 5900, 5938, 8080, 10000, 10050, 20000]
+small = [25,80,53,443,445,3389, 8080,8443]
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="ip address to scan for")
-    parser.add_argument('iface',type=str,help="enter the interface aka dev")
-    parser.add_argument('ip_addr',type=str,help="enter ")
+    parser.add_argument('-i',dest='iface',type=str,help="enter the interface aka dev", required=True)
+    parser.add_argument('-d',dest='ip_addr',type=str,help="enter ",required=True)
+    parser.add_argument('-l',dest="ports",help="big or small list of ports - b or s ")
     args=parser.parse_args()
     return args
 
-def scan_m(ip,iface):
+def scan_m(ip,iface,ports):
     print('[+] start scaning.please wait...\n')
-    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), iface=iface,timeout=1,verbose=False)
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), iface=iface,timeout=0.1,verbose=False)
     if ans:
         print("  IP\t\tMAC Address\t\tOpen ports\n----------------------------------------------------------------------->")
+        if ports == 'b':
+            ports=big
+        else:
+            ports=small
+        
         for (s,r,) in ans:
-            a,u = sr(IP(dst=r[ARP].psrc)/TCP(sport=33333,dport=ports_big,flags="S"),iface=iface,timeout=2,verbose=False)
+            a,u = sr(IP(dst=r[ARP].psrc)/TCP(sport=33333,dport=ports,flags="S"),iface=iface,timeout=3,verbose=False)
             if a:
                 open_ports=[]
                 for (s1,r1) in a:
@@ -35,14 +42,16 @@ def scan_m(ip,iface):
 
 def syn_scan(ip,iface):
     print ('[+] making syn scan...')
-  
+    
 def main():
     args = get_arguments()
-    try:
-        scan_m(args.ip_addr,args.iface)
-        print('[+] stop scaning.\n')
-    except PermissionError:
-        print('\n\r[-] sorry...u need sudo privilege...')
-    except KeyboardInterrupt:
-        print('\n\r[-] Detected CTRL+C. Stop scaning')
+    if re.search('((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}',args.ip_addr):
+        try:
+            scan_m(args.ip_addr,args.iface,args.ports)
+            print('[+] stop scaning.\n')
+        except PermissionError:
+            print('\n\r[-] sorry...u need sudo privilege...')
+    else:
+        print('[-] ip not valid')
+
 main()
